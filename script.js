42aeaea9ded44671d27a96a070352fd05e6ded54
Sai Teja <saicharanteja.733@gmail.com>
2025-11-11 14:50:40 +0530

    reemoved toggle

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector(".menu-toggle");
-const nav = document.querySelector("nav");
-
-menuBtn.addEventListener("click", () => {
-  nav.classList.toggle("active");
-});
